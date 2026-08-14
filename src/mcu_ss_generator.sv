6114e488733b5ca35ee2f165a223dad5d4bcd27f
Hyzeth <suleimanovr0sab@gmail.com>
2026-08-15 01:01:41 +0300

    SS переделан на LFSR, теперь в однополосном режиме не слышно тонов около 8 МГц

--- src/mcu_ss_generator.sv
+++ src/mcu_ss_generator.sv
@@ -4,38 +4,21 @@ module mcu_ss_generator
     output reg ss_clk_out       // 8 MHz + 1/32
 );
 
-reg [8:0]f_sweep;      // 199.8 MHz/2^15 = 6.1 kHz
 wire [19:0]f0;
-assign f0 = 20'd42000;  // 2^20/125M*8M
-reg [19:0]ph_acc;       // ~32x f_sweep
+assign f0 = 20'd42000;  // 2^20/199.8M*8M
+reg [19:0]ph_acc;
 reg [19:0]f;
 
-wire sweep_top;
-wire sweep_bottom;
-assign sweep_top = (&f_sweep) ? 1:0;
-assign sweep_bottom = (~|f_sweep) ? 1:0;
-
-reg dir;
-
-always @ (posedge sweep_top or posedge sweep_bottom)
-begin
-    if(sweep_top) dir <= 0;
-    else
-    if(sweep_bottom) dir <= 1;
-end
-
-always @ (posedge ss_clk_out)
-begin
-    if(dir == 1) f_sweep <= f_sweep + 1;
-    else f_sweep <= f_sweep - 1;
-end
+reg [19:0]lfsr;
+wire lfsr_fb;
+assign lfsr_fb = (lfsr[19]^lfsr[2]) || ~|lfsr;
 
 always @ (posedge clk_H)
 begin
 
-    
+    lfsr <= {lfsr[18:0], lfsr_fb};
 
-    f <= f0 + (f_sweep >> 2);
+    f <= f0 + lfsr[12:0];
     ph_acc <= ph_acc - f;
     ss_clk_out <= ph_acc[19];
 
