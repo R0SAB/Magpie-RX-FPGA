f626398efcf0a1ada5eff7995dc9c8889eb86f16
Hyzeth <suleimanovr0sab@gmail.com>
2026-08-21 23:32:15 +0300

    Ошибка фазы теперь добавляется прямо в аккумулятор (до этого дважды усекалась)

--- src/lo_sync.sv
+++ src/lo_sync.sv
@@ -10,7 +10,7 @@ module lo_sync
 
 
 localparam FREQ_TAU_BITS = 12;
-localparam PHASE_INJECT_BITS = 12;
+localparam PHASE_INJECT_BITS = 4;
 
 wire signed [23:0]narrow_I;
 wire signed [23:0]narrow_Q;
@@ -65,16 +65,13 @@ reg signed [23:0]phase_diff_prev;
 wire signed [23:0]freq_diff;
 assign freq_diff = phase_diff - phase_diff_prev;
 
-wire signed [23:0]full_error;
-assign full_error = freq_diff + (phase_diff >>> (24-PHASE_INJECT_BITS));
-
 wire signed [1+16+FREQ_TAU_BITS:0]itgr_next;
-assign itgr_next = itgr + full_error;
+assign itgr_next = itgr + freq_diff;
 
 always @ (posedge clk_44k)
 begin
 
-    ph_acc <= ph_acc + f0;
+    ph_acc <= ph_acc + f0 + (phase_diff >>> (24-PHASE_INJECT_BITS));
 
     if(itgr_next > (1 <<< (16+FREQ_TAU_BITS))) itgr <= (1 <<< (16+FREQ_TAU_BITS));
     else
