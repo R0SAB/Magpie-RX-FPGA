2d59e99a71c61f44f55a709098404e43106db86b
Hyzeth <you@example.com>
2026-08-22 17:40:17 +0300

    Коррекция фазы перенесена в ограничитель интегратора

--- src/lo_sync.sv
+++ src/lo_sync.sv
@@ -66,7 +66,7 @@ wire signed [23:0]freq_diff;
 assign freq_diff = phase_diff - phase_diff_prev;
 
 wire signed [1+16+FREQ_TAU_BITS:0]itgr_next;
-assign itgr_next = itgr + freq_diff;
+assign itgr_next = itgr + freq_diff - phase_correction;
 
 wire signed [23:0]phase_correction;
 assign phase_correction = phase_diff >>> (24-PHASE_INJECT_BITS);
@@ -80,7 +80,7 @@ begin
     else
     if(itgr_next < -(1 <<< (16+FREQ_TAU_BITS))) itgr <= -(1 <<< (16+FREQ_TAU_BITS));
     else
-    itgr <= itgr_next - phase_correction;
+    itgr <= itgr_next;
 
     phase_diff_prev <= phase_diff;
 
