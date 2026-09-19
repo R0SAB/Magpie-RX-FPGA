516126eb864e640adee7e166d4695decb8acbfe1
Hyzeth <you@example.com>
2026-09-19 14:20:43 +0300

    Исправлена разрядность АРУ

--- src/agc.sv
+++ src/agc.sv
@@ -14,16 +14,18 @@ typedef enum integer {MOD_SSB = 0, MOD_AM = 1} modulation_enum;
 
 reg [1:0]clk_44k_eg;
 
-reg signed [23:0]abs_in;
 reg signed [23:0]abs_out;
 reg signed [23:0]detector_qpeak;
 logic signed [23:0]target;
 
-localparam GAIN_TAU_BITS = 15;
+localparam GAIN_TAU_BITS = 10;
 
 reg signed [15+GAIN_TAU_BITS:0]gain_itgr;
 wire signed [15:0]gain;
 assign gain[15:0] = gain_itgr >>> GAIN_TAU_BITS;
+
+wire signed [15+GAIN_TAU_BITS:0]gain_itgr_next;
+assign gain_itgr_next = gain_itgr + target - detector_qpeak;
 
 always_comb
 begin
@@ -36,7 +38,7 @@ begin
     end
 end
 
-reg signed [35:0]multiplier;
+reg signed [39:0]multiplier;
 
 
 always @ (posedge clk_70M)
@@ -47,20 +49,19 @@ begin
     if(clk_44k_eg == 2'b01)
     begin
 
-        if(audio_in[23]) abs_in <= -audio_in;
-        else abs_in <= audio_in;
-
         if(audio_out[23]) abs_out <= -audio_out;
         else abs_out <= audio_out;
 
         if(detector_qpeak < abs_out) detector_qpeak <= detector_qpeak + (abs_out - detector_qpeak);
         else detector_qpeak <= detector_qpeak - (detector_qpeak >>> 7);
         
-        gain_itgr <= gain_itgr + target - detector_qpeak;
-        if(gain_itgr > ('b11 <<< 27)) gain_itgr <= ('b11 <<< 27);
+        //gain_itgr <= gain_itgr + target - detector_qpeak;
+        //if(gain_itgr > ('b11 <<< 27)) gain_itgr <= ('b11 <<< 27);
+
+        if(gain_itgr_next < (1 <<< (15+GAIN_TAU_BITS-2))) gain_itgr <= gain_itgr_next;
 
         multiplier <= gain * audio_in;
-        audio_out[23:0] <= multiplier[35:12];
+        audio_out[23:0] <= multiplier[39:16];
 
     end
 
